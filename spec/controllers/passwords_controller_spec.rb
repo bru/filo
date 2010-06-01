50bfb2df0be1ab44649babe2b99ebd53e8ce9760
Riccardo Cambiassi <bru@calimero.config>
2010-06-02 00:07:34 +0100

    User Nav [#3735521]
    * Greeting (to nick or email)
    * Settings link
    * Sign out
    ot
    * Sign in
    * Sign up

--- spec/controllers/passwords_controller_spec.rb
+++ spec/controllers/passwords_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe SessionsController do
+describe PasswordsController do
 
   #Delete this example and add some real ones
-  it "should use SessionsController" do
-    controller.should be_an_instance_of(SessionsController)
+  it "should use PasswordsController" do
+    controller.should be_an_instance_of(PasswordsController)
   end
 
 end
