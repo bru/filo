6f976444f5d77f8d36fc0724e4d6364dbfdae3b1
Riccardo Cambiassi <riccardo@headshift.com>
2010-06-05 00:20:00 +0100

    Set Locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+
+  # include Clearance
   include Clearance::Authentication
+  
+  # set locale
+  before_filter :set_locale
+  
   helper :all # include all helpers, all the time
+  
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   
   # Scrub sensitive parameters from your log
@@ -11,11 +18,29 @@ class ApplicationController < ActionController::Base
 
   # Respond to iphone requests
   acts_as_iphone_controller
+  
   private
   
   def deny_action(flash_message = nil)
     flash[:alert] = flash_message if flash_message
     redirect_to root_path
+  end
+  
+  def set_locale
+    I18n.locale = extract_locale_from_params || extract_locale_from_session || extract_locale_from_user
+  end
+  
+  def extract_locale_from_params
+    session[:locale] = params[:locale] if params[:locale]
+  end
+  
+  # being a personal service, we're storing the locale in the session. YES, we're breaking the web
+  def extract_locale_from_session
+    session[:locale] || nil
+  end
+  
+  def extract_locale_from_user
+    signed_in? && current_user.language
   end
   
 end
