327dd8e7f1facd0e1ec7a99cca74da006dddf069
Riccardo Cambiassi <riccardo@headshift.com>
2010-06-05 14:47:15 +0100

    properly handle clicks in iphone interface

--- public/javascripts/iphone.js
+++ public/javascripts/iphone.js
@@ -1,19 +1,21 @@
 var jQT = new $.jQTouch({
         statusBar: 'black-translucent',
-        // icon: false,
+        icon: false,
         useFastTouch: true,
         useAnimations: false,
         preloadImages: [
             '/stylesheets/jqtouch/jqt/img/chevron.png',
             '/stylesheets/jqtouch/jqt/img/back_button.png',
             '/stylesheets/jqtouch/jqt/img/back_button_clicked.png',
+            '/stylesheets/jqtouch/jqt/img/button.png',
             '/stylesheets/jqtouch/jqt/img/button_clicked.png',
             '/stylesheets/jqtouch/jqt/img/chevron_circle.png',
+            '/stylesheets/jqtouch/jqt/img/toolbar.png'
             ]
     });
     
 $(function() {
-  $('#home ul.unread li.knot a[target="_blank"]').tap(function() {
-    $(this).parent('li').appendTo('#read ul.read');
+  $('ul.unread li.knot a[target="_blank"]').click(function() {
+    $(this).parent('li').detach().appendTo('#read ul.read').end().click();
   });
 });
