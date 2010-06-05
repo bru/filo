d20e7136259ab7490d6de76ae9f0e9e6a9c5c089
Riccardo Cambiassi <riccardo@headshift.com>
2010-06-05 14:46:04 +0100

    Properly redirect from / if iphone

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,9 +1,9 @@
 class StaticController < ApplicationController
+  before_filter :redirect_if_iphone
+  
   def home
     if signed_in?
       redirect_to knots_path 
-    elsif is_iphone_request?
-      redirect_to iphone_path 
     end
   end
 
@@ -11,6 +11,12 @@ class StaticController < ApplicationController
   end
 
   def contacts
+  end
+  
+  protected
+  
+  def redirect_if_iphone
+    redirect_to iphone_path if is_iphone_request?
   end
 
 end
