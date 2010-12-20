bf0f7350645cea1fb28b37846afd1c66051fd224
Riccardo Cambiassi <riccardo@headshift.com>
2010-12-20 22:35:01 +0000

    First run of rails generator + customization of Gemfile.
    N.B. tests still fail, apparently because of Clearance. I tried upgrading to the latest version of the gem but it still has troubles.
    Given the fact this project already bent clearance a bit, I may give something different (like devise or authlogic) a try.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Filo::Application.config.secret_token = '062a1deba45c7a428a6f78f5352b7bafab2870a14f0d8aa811146aded2282358cc42c121f940a69dd4e839ba6c35dd43e48b0e92b254084f67107c9f7de235f3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-ActionController::Routing::Routes.draw do |map|
-
+Filo::Application.routes.draw do |map|
+  
   map.iphone '/i', :controller => "iphone", :action => "index"
   
   map.addknot "/a", :controller => "knots", :action => "create", :remote => true  
@@ -18,46 +18,65 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resource :session, :controller => 'sessions', :only => [:new, :create, :destroy]
   Clearance::Routes.draw(map)
-  # The priority is based upon order of creation: first created -> highest priority.
+
+  map.root :controller => "static", :action => "home"
+
+  Translate::Routes.translation_ui(map) if RAILS_ENV == "development"
+  
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
 
   # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+  #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
+  #   resources :products
 
   # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
 
   # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
   # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
   #   end
 
   # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
   #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "static", :action => "home"
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing or commenting them out if you're using named routes and resources.
-  Translate::Routes.translation_ui(map) if RAILS_ENV == "development"
-  # map.connect ':controller/:action/:id'
-  # map.connect ':controller/:action/:id.:format'
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id(.:format)))'
 end
